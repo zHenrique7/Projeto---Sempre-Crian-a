c2045d5eddcb34262065a1a7e3fade757a151424
AdrielP99 <adriel.paixao@soulasalle.com.br>
2024-06-11 10:02:52 -0300

    Add files via upload

--- teste_valida_adriel.py
+++ teste_valida_adriel.py
@@ -25,7 +25,7 @@ def valida_nome(self,nome):
         valida=False
     return valida
 
-def telefone(self,telefone):
+def valida_telefone(self,telefone):
     if len(telefone)!=11:
         messagebox.showwarning('Telefone inválido','Digite um telefone válido com 11 dígitos no modelo 99999999999')
         return False
@@ -33,3 +33,26 @@ def telefone(self,telefone):
         if(not(i.isdigit())):
             messagebox.showwarning('Telefone inválido','Só podem haver números no telefone')
             return False
+    return True
+
+def valida_cpf(self,cpf):
+    if len(cpf)!=14:
+        messagebox.showwarning('CPF inválido','Digite um cpf no modelo 123.123.123-12')
+        return False
+    for i in range(len(cpf)):
+        if(i==3 and cpf[i]!='.'):
+            messagebox.showwarning('CPF inválido','Digite um cpf no modelo 123.123.123-12')
+            return False
+        elif(i==7 and cpf[i]!='.'):
+            messagebox.showwarning('CPF inválido', 'Digite um cpf no modelo 123.123.123-12')
+            return False
+        elif(i==11 and cpf[i]!='-'):
+            messagebox.showwarning('CPF inválido', 'Digite um cpf no modelo 123.123.123-12')
+            return False
+        elif(i!=3 and i!=7 and i!=11):
+            messagebox.showwarning('CPF inválido', 'Digite um cpf no modelo 123.123.123-12')
+            return False
+        elif(not(cpf[i].isdigit())):
+            messagebox.showwarning('CPF inválido', 'Digite um cpf no modelo 123.123.123-12')
+            return False
+    return True
