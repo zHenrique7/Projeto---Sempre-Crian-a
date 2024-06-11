21fe5658da7c0dde5b304f08fd29779bc07b0e52
AdrielP99 <adriel.paixao@soulasalle.com.br>
2024-06-11 09:44:50 -0300

    Add files via upload

--- teste_valida_adriel.py
+++ teste_valida_adriel.py
@@ -1,4 +1,4 @@
-from tkinter import messagebox as mb
+from tkinter import messagebox
 from tkinter import filedialog, ttk, font, messagebox
 from tkinter.ttk import Treeview
 import customtkinter
@@ -10,17 +10,26 @@ import mysql.connector
 import ctypes
 from datetime import datetime
 
-def valida_nome(nome):
+def valida_nome(self,nome):
     valida=True
     if nome=='':
-        mb.showwarning('Campos vazios','Campo nome é obrigatório!')
+        messagebox.showwarning('Campos vazios','Campo nome é obrigatório!')
         valida=False
     for i in nome:
-        if (not i.isalpha() or not i.isspace()):
-            mb.showwarning('Campo inválido','Caractere invalido')
+        if (not (i.isalpha()) and not (i.isspace())):
+            messagebox.showwarning('Campo inválido','Caractere invalido')
             valida=False
             break
     if (len(nome)>50):
-        mb.showwarning('Passou do total','Nome muito grande')
+        messagebox.showwarning('Passou do total','Nome muito grande')
         valida=False
     return valida
+
+def telefone(self,telefone):
+    if len(telefone)!=11:
+        messagebox.showwarning('Telefone inválido','Digite um telefone válido com 11 dígitos no modelo 99999999999')
+        return False
+    for i in telefone:
+        if(not(i.isdigit())):
+            messagebox.showwarning('Telefone inválido','Só podem haver números no telefone')
+            return False
